2e6e757a88c6f421bd8a3f6b0a929291fd430796
VladiusVostokus <kovalenkov222@gmail.com>
2025-05-09 18:32:52 +0300

    Create collection with device class links
    This collection will be used like a strategy collection for creation of
    device instanses inside factory or builder.

---
+++ src/devices/devicesCollection.ts
@@ -0,0 +1,8 @@
+import { Light } from "./Light.js";
+import { Thermostat } from "./Thermostat.js";
+
+export const devicesCollection = {
+    'light': Light,
+    'thermostat': Thermostat,
+    'something_else':'add more devices here'
+}
