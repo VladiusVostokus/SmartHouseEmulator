34f24dea5e6e3331aa204b5ad1489ad73196a4bd
VladiusVostokus <kovalenkov222@gmail.com>
2025-05-09 18:26:31 +0300

    Add stub light implementation
    Create class Light that implements IDevice, remove fields from IDevice to make
    them private in Light class. I create stub to have class Light, which I'll use
    in JS collection for Builder or Factory implementation.

--- src/interfaces/IDevice.ts
+++ src/interfaces/IDevice.ts
@@ -1,10 +1,4 @@
-import type { ICommunicator } from "./ICommunicator.js";
-
 export interface IDevice{
-    name: string;
-    type: string;
-    isOn: boolean;
-    communicator: ICommunicator;
     subscribe(topic: string): void;
     publish(topic: string): void;
     turnOn(): void;
