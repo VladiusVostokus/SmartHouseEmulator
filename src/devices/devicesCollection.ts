f64a93f80bab92a45afacccb2f83a1744fe2ac6f
VladiusVostokus <kovalenkov222@gmail.com>
2025-05-10 12:07:25 +0300

    Create device factory
    Factory that gets type of device from record of device classes, creates it
    and return from factory method. Also change object to record so it can work.

--- src/devices/devicesCollection.ts
+++ src/devices/devicesCollection.ts
@@ -1,8 +1,12 @@
+import type { ICommunicator } from "../interfaces/ICommunicator.js";
+import type { IDevice } from "../interfaces/IDevice.js";
 import { Light } from "./Light.js";
 import { Thermostat } from "./Thermostat.js";
 
-export const devicesCollection = {
+export type DeviceConstructor = new (name: string, communicator: ICommunicator) => IDevice;
+
+export const devicesCollection: Record<string, DeviceConstructor > = {
     'light': Light,
     'thermostat': Thermostat,
-    'something_else':'add more devices here'
-}
+    //'something_else':'add more devices here'
+};
