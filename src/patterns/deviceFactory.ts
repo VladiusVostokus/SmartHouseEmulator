e1e8a144d23a4b2380f819c962bdb9642002f3b5
Illia Kornyk <krekerbrow@gmail.com>
2025-05-11 17:07:00 +0300

    feat: setup eslint and prettier

--- src/patterns/deviceFactory.ts
+++ src/patterns/deviceFactory.ts
@@ -3,15 +3,19 @@ import type { ICommunicator } from "../interfaces/ICommunicator.ts";
 import type { IDevice } from "../interfaces/IDevice.ts";
 
 export class DeviceFactory {
-    deviceCollection: Record<string, DeviceConstructor>
+  deviceCollection: Record<string, DeviceConstructor>;
 
-    constructor(devices: Record<string, DeviceConstructor>) {
-        this.deviceCollection = devices
-    }
+  constructor(devices: Record<string, DeviceConstructor>) {
+    this.deviceCollection = devices;
+  }
 
-    createDevice(deviceType : string, name: string, communicator :ICommunicator) : IDevice {
-        const Device = this.deviceCollection[deviceType];
-        if (!Device) throw new Error(`Unknown device type: ${deviceType}`);
-        return new Device(name, communicator);
-    }
+  createDevice(
+    deviceType: string,
+    name: string,
+    communicator: ICommunicator,
+  ): IDevice {
+    const Device = this.deviceCollection[deviceType];
+    if (!Device) throw new Error(`Unknown device type: ${deviceType}`);
+    return new Device(name, communicator);
+  }
 }
