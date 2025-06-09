dd761d89eabda0a2b052d6585bc2b4330900c98d
Illia Kornyk <krekerbrow@gmail.com>
2025-06-09 12:08:10 +0300

    refactor: naming convention

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -3,3 +3,6 @@ export * from "./interfaces/communicator.interface.js";
 export * from "./classes/communicator.class.js";
 export * from "./classes/base-device.class.js";
 export * from "./types/command-payload.type.js";
+export * from "./types/message.type.js";
+export * from "./types/message-payload.type.js";
+export * from "./types/topic-template.type.js";
