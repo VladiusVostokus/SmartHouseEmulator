1563ab2020ad9b4ee454490f849e798d037bad17
VladiusVostokus <kovalenkov222@gmail.com>
2025-06-09 10:52:05 +0300

    refactor: use topics collection, instead of hardcoded topic templates

--- packages/common/src/classes/Communicator.ts
+++ packages/common/src/classes/Communicator.ts
@@ -1,14 +1,22 @@
 import mqtt from "mqtt";
-import type { ICommunicator } from "../interfaces/ICommunicator.js";
+import type { ICommunicator, TopicTemplate } from "@smart-house/common";
 
 export class MQTTCommunicator implements ICommunicator {
   private client: mqtt.MqttClient;
   private clientId: string;
+  private topics: TopicTemplate = {
+    action: '',
+    status: ''
+  };
 
   constructor(topics: string[], clientId: string, brocker: string) {
     this.clientId = clientId;
+
+    this.topics['action'] = topics[0].replace('+',clientId);
+    this.topics['status'] = topics[1].replace('+',clientId);
+
     const willMessage = {
-      topic: `/home/${this.clientId}/status`,
+      topic: this.topics['status'],
       payload: JSON.stringify({ status: "offline" }),
       retain: true,
     };
@@ -24,7 +32,7 @@ export class MQTTCommunicator implements ICommunicator {
     this.client.on("connect", () => {
       console.log("Connected to broker");
       this.client.publish(
-        `/home/${this.clientId}/status`,
+        this.topics['status'],
         JSON.stringify({ clientId: this.clientId, status: "online" }),
         {
           qos: 1,
@@ -46,10 +54,10 @@ export class MQTTCommunicator implements ICommunicator {
       status: status,
     };
     this.client.publish(
-      `/home/${this.clientId}/action`,
+      this.topics['action'],
       JSON.stringify(payload),
     );
-    console.log("Publish data to topic:", `/home/${this.clientId}/action`);
+    console.log("Publish data to topic:", this.topics['action']);
   }
 
   listen(handler: (arg0: string, arg1: Buffer) => void) {
